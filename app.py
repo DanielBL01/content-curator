dfd6881045e6f5cc9a96483055dd5642cc0e0486
DanielBL01 <dblee0532@gmail.com>
2021-03-27 16:56:07 -0400

    basic functions

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
+from utils import parseURL, contentCurator
 import redis 
 
 app = Flask(__name__)
 cache = redis.Redis(host='localhost', port=6379, db=0)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
+    if request.method == 'POST':
+        return redirect(url_for('content', url=request.form['url']))
+
     return render_template('homepage.html')
 
 @app.route('/content')
 def content():
-    return render_template('content.html')
+    content = parseURL.parse(request.args['url'])
+    if content == '':
+        return render_template('error.html')
+        
+    return render_template('content.html', content=content)
 
 if __name__ == '__main__':
     app.run(debug=True)
